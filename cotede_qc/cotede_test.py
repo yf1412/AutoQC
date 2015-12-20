e229894279e4cff1b733f1cb1953f170e317fac5
Gui <guilherme@castelao.net>
2015-12-19 23:33:11 -0800

    CoTeDe's preset QC are build in.
    If you choose between one of the preset QC configurations, the setup is
    available inside CoTeDe, so there is no need to provide it, neither keep
    a copy of it.

--- cotede_qc/cotede_test.py
+++ cotede_qc/cotede_test.py
@@ -92,10 +92,8 @@ def get_qc(p, config, test):
     # Check if we need to perform the quality control.
     if p.uid() != cotede_results[0] or config != cotede_results[1]:
         inputs = DummyCNV(p)
-        with open('cotede_qc/qc_cfg/' + config + '.json') as f:
-            cotede_results = [p.uid(), 
-                              config, 
-                              ProfileQC(inputs, cfg=json.load(f))]
+        cotede_results = [p.uid(), config,
+                ProfileQC(inputs, cfg=config)]
     
     # Define where the QC results are found.
     if test == 'location_at_sea':
